2570622dabad367ae92a587ef7d42d4467a0c112
OSTERITE <adrian@bandidos.org>
2023-02-26 20:05:33 +0100

    added many more functions

--- function/script.js
+++ function/script.js
@@ -39,7 +39,25 @@ function random_integer_in_range(lower_limit, upper_limit){
     return Math.floor( (Math.random() * (upper_limit - lower_limit)) + lower_limit)
 }
 
+function largest(number_1, number_2){
+    if (number_1 >= number_2){
+      return number_1
+    }
+    return number_2
+  }
+  
+  function smallest(number_1, number_2){
+    if (number_1 <= number_2){
+      return number_1
+    }
+    return number_2
+  }
 
+function clear_all_intervals(){
+    for (let i = 0; i < 100; i++) {
+      window.clearInterval(i)    
+    }
+  }
 
 //---------------------------MATH------------------------------
 
@@ -66,18 +84,63 @@ function v2a(vector1, vector2, unit) {
     }
 }
 
-  
-
 
 //---------------------CANVAS, DOM-----------------------
+
+function fill_largest_font_centered(canvas, text, font, y_postion, color){
+  
+    const ctx = canvas.getContext("2d");
+    let font_size = largest_font_size(canvas, text, font)
+    const canvas_center_x = canvas.width / 2;
+    
+    ctx.font = `${font_size}px ${font}`;
+    ctx.fillStyle = color
+    ctx.textAlign = "center";
+    ctx.fillText(text, canvas_center_x, y_postion);
+}
+  
+  function largest_font_size(canvas, text, font){
+    
+    const temporary_canvas = document.createElement('canvas');
+    const temporary_ctx = temporary_canvas.getContext("2d");
+    temporary_canvas.width = canvas.width;
+    temporary_canvas.height = canvas.height;
+  
+    let font_size = smallest(canvas.height, canvas.width)
+  
+    temporary_ctx.font = `${font_size}px ${font}`;
+    let text_metrics = temporary_ctx.measureText(text);
+  
+    //algorithm to quickly find the largest possible font size
+    while (text_metrics.width > canvas.width){
+      font_size *= 0.9
+      temporary_ctx.font = `${font_size}px ${font}`;
+      text_metrics = temporary_ctx.measureText(text);
+    }
+    while (text_metrics.width + 1 < canvas.width){
+      font_size += 1
+      temporary_ctx.font = `${font_size}px ${font}`;
+      text_metrics = temporary_ctx.measureText(text);
+    }
+    return font_size
+}
+
+function draw_background(canvas, color){
+    ctx = canvas.getContext("2d");
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+  
+
+function create_hsl_expression(hue, saturation, lightness){
+    return `hsl( ${hue}, ${saturation}%, ${lightness}%)`
+}
+
 function text_to_element(text, element){
     element.innerHTML = text
-  }
+}
 
-
-//---------------------CANVAS-----------------------
-
-function get_cursor_position(canvas, event) {
+function get_cursor_position(event) {
 //finds the absolute coordinates clicked, given as distence from top left.
 return [event.offsetX, event.offsetY];
 }
@@ -103,22 +166,6 @@ function draw_square(canvas_info, background_img, cursor_start_x, cursor_end_x, 
         ctx.stroke();
     }
 }
-// function draw_square(canvas_info, background_img, cursor_start_x, cursor_end_x, cursor_start_y, cursor_end_y) {
-//     const canvas = canvas_info;
-//     const ctx = canvas.getContext("2d", { alpha: false });
-//     const current_square = largest_drawable_square(cursor_start_x, cursor_end_x, cursor_start_y, cursor_end_y);
-
-//     const parameter_x = cursor_start_x + ~~current_square.width;
-//     const parameter_y = cursor_start_y + ~~current_square.height;
-
-//     //Draws the guiding box if it fits the canvas
-//     if ( (0 < parameter_x && parameter_x < canvas.width) && (0 < parameter_y && parameter_y < canvas.height ) ) {
-//         ctx.drawImage(background_img, 0, 0, canvas.width, canvas.height);
-//         ctx.beginPath();
-//         ctx.rect( cursor_start_x, cursor_start_y, current_square.width, current_square.height );
-//         ctx.stroke();
-//     }
-// }
 
 function largest_drawable_square(start_x, end_x, start_y, end_y) {
     let height = 0; //Positive values down, negative values up.
@@ -172,8 +219,4 @@ function largest_drawable_square(start_x, end_x, start_y, end_y) {
 
 async function load(file_name) {
     return await fetch(file_name).then((response) => response.text() );
-}
-
-function load(file_name) {
-    return fetch(file_name).then((response) => response.text() );
 }
