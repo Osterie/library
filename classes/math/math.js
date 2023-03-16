8c7a020cb997cafe25bf2a6c904a7839b082f514
OSTERITE <adrian@bandidos.org>
2023-03-16 14:27:41 +0100

    chassis for room_geometry classes

---
+++ classes/math/math.js
@@ -0,0 +1,44 @@
+class Vector_2d{
+    
+    constructor(){
+
+    }
+}
+
+
+class Vector_3d{
+
+
+    constructor(){
+        this.vector 
+        this.length
+    }
+
+    add_vector(vector){
+        this.vector = vector
+    }
+
+
+    create_vector_from_points(){
+
+    }
+}
+
+const test_vector = new Vector_3d
+
+
+
+class Room_geometry{
+
+    constructor(){
+
+    }
+
+    sum_vectors(vector_1, vector_2){
+       return  
+    }
+
+    sub_vectors(vector_1, vector_2){
+        return
+    }
+}
