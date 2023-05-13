9a47b973b4c5209c2c56873faf2b33a571d5e76b
OSTERITE <adrian@bandidos.org>
2023-05-13 22:59:26 +0200

    added function

--- functions/elements.js
+++ functions/elements.js
@@ -1,3 +1,11 @@
 function text_to_element(text, element){
     element.innerHTML = text
 }
+
+function animasjon(element, fromX, fromY, toX, toY, time=2000) {
+    element.animate([
+                    {left: `${fromX}px`, top: `${fromY}px`, opacity: 0}, // Fra
+                    {left: `${toX}px`, top: `${toY}px`, opacity: 1}      // Til
+                    ], 
+                 time); // Tid
+}
